a4b8295772862616ac7e218f8303e2f23534e57a
Lucas Neumann <lucas.neumann@nubank.com.br>
2018-09-14 16:55:19 -0300

    lint index.js

--- pages/index.js
+++ pages/index.js
@@ -1,83 +1,80 @@
-import InputContainer from "../components/InputContainer";
-import OutPutContainer from "../components/OutPutContainer";
-import Header from "../components/Header";
-import React, { Component } from 'react'
-import { toCurrency, getRetirementResults } from '../utils/math'
-import { isNumber } from '../utils/input'
+import React, { Component } from 'react';
+import InputContainer from '../components/InputContainer';
+import OutPutContainer from '../components/OutPutContainer';
+import Header from '../components/Header';
+import { getRetirementResults } from '../utils/math';
+import { isNumber } from '../utils/input';
 // import Tachyons from 'tachyons'
 
 class Index extends Component {
-     constructor(props) {
-        super(props);
-        this.state = {
-            isShowingIntro: "true",
-            myCurrentIncome: 10000,
-            myCurrentBalance: 100000,
-            myCurrentAge: 27,
-            myCurrentMonthlySavings: 1500,
-            myCurrentLifestyleCost: 3000,
-            myRetirementAge: 0,
-            myAnnualInterestRate: 0.08,
-            myRetirementIncome: 10000,
-            myLifeExpectancy: 100,
-            myInvestiments: [
-              {
-                label: 'poupança',
-                rate: 0.030
-              },
-              {
-                label: 'renda fixa',
-                rate: 0.060
-              },
-              {
-                label: 'renda variável',
-                rate: 0.085
-              }
-            ],
-            lifeEvents: [
-              {
-                label: 'viagem',
-                age: 30, // years, not months
-                cost: 100000
-              },
-            ],
-            retirementResults: false,
-         }
-    }
+  constructor(props) {
+    super(props);
+    this.state = {
+      isShowingIntro: 'true',
+      myCurrentIncome: 10000,
+      myCurrentBalance: 100000,
+      myCurrentAge: 27,
+      myCurrentMonthlySavings: 1500,
+      myCurrentLifestyleCost: 3000,
+      myRetirementAge: 0,
+      myAnnualInterestRate: 0.08,
+      myRetirementIncome: 10000,
+      myLifeExpectancy: 100,
+      myInvestiments: [
+        {
+          label: 'poupança',
+          rate: 0.030,
+        },
+        {
+          label: 'renda fixa',
+          rate: 0.060,
+        },
+        {
+          label: 'renda variável',
+          rate: 0.085,
+        },
+      ],
+      lifeEvents: [
+        {
+          label: 'viagem',
+          age: 30, // years, not months
+          cost: 100000,
+        },
+      ],
+      retirementResults: false,
+    };
+  }
 
     handleInput = (e, floatValue, maskedValue) => {
+      // remove picture on first edit
+      document.getElementById('chartContainer').scrollIntoView({
+        behavior: 'smooth',
+        block: 'start',
+      });
 
-        //remove picture on first edit
-        document.getElementById('chartContainer').scrollIntoView({
-            behavior: "smooth",
-            block: "start"
-        })
+      // save state in a variable
+      const { state } = this;
 
-        // save state in a variable
-        let state = this.state
-
-        // check if is special currencyInput
-        if(floatValue && maskedValue){
-            state[e.target.id] = floatValue
-            this.setState(state)
-        } else {
+      // check if is special currencyInput
+      if (floatValue && maskedValue) {
+        state[e.target.id] = floatValue;
+        this.setState(state);
+      } else if (e.target.type) {
         // update state for every input field
-        if (e.target.type){
-            state[e.target.id] = e.target.value
-            this.setState(state)
-        } else {
-            state[e.target.parentNode.parentNode.querySelectorAll('input')[0].id] = e.target.parentNode.parentNode.querySelectorAll('input')[0].value
-            this.setState(state)
-        }
+        state[e.target.id] = e.target.value;
+        this.setState(state);
+      } else {
+        state[e.target.parentNode.parentNode.querySelectorAll('input')[0].id] = e.target.parentNode.parentNode.querySelectorAll('input')[0].value;
+        this.setState(state);
+      }
+
+
+      // calculate retirement age
+      this.setState({ retirementResults: getRetirementResults(this.state) });
     }
 
-        // calculate retirement age
-        this.setState({retirementResults : getRetirementResults(this.state)})
-    }
-
-    handleTableInput = (idx, tableName, table, textField = false) => event => {
-
-      const value = event.target.value;
+    handleTableInput = (idx, tableName, table, textField = false) => (event) => {
+      const { value } = event.target;
       const field = event.target.id;
 
       if (isNumber(value) || textField) {
@@ -93,29 +90,30 @@ class Index extends Component {
         this.setState(prevState => ({
           [tableName]: updatedTable,
           retirementResults: getRetirementResults(
-            {...prevState, [tableName]: updatedTable})
-        }))
+            { ...prevState, [tableName]: updatedTable },
+          ),
+        }));
       }
     };
 
     handleAddTableRow = (tableName, fields) => () => {
-
       this.setState({
         [tableName]: [...this.state[tableName], fields],
       });
     };
 
     handleRemoveTableRow = (idx, tableName, table) => () => {
-      const updatedTable = table.filter((p, pidx) => idx !== pidx)
+      const updatedTable = table.filter((p, pidx) => idx !== pidx);
       this.setState(prevState => ({
         [tableName]: updatedTable,
         retirementResults: getRetirementResults(
-          {...prevState, [tableName]: updatedTable})
-      }))
+          { ...prevState, [tableName]: updatedTable },
+        ),
+      }));
     };
 
     render() {
-        return (
+      return (
         <div>
             <Header />
             <div className="flex w-100 vh-100 bg-light-pink dark-blue">
@@ -144,7 +142,7 @@ class Index extends Component {
                 .dark-blue{color: #0148B3}
                 `
             }</style>
-        </div> );
+        </div>);
     }
 }
 
