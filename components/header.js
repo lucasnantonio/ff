7cb908732e5ad3a7991789c2e50cf5b0964b08c0
Lucas Neumann <lucas.neumann@nubank.com.br>
2018-09-14 14:26:56 -0300

    functional component for header

--- components/header.js
+++ components/header.js
@@ -1,25 +1,17 @@
-import {Component} from 'react'
+const Header = () => (
+        <header>
+            <link rel="stylesheet" href="https://unpkg.com/tachyons@4.10.0/css/tachyons.min.css"/>
+            <link href="https://fonts.googleapis.com/css?family=Poppins:300,700" rel="stylesheet"></link>
+            <style jsx global>
+                {`
+                    body *{
+                        // font-family: 'Rubik', sans-serif;
+                        font-family: 'Poppins', sans-serif;
+                        font-weight: 100;
+                    }
+                `}
+            </style>
+        </header>
+);
 
-class Header extends Component {
-    state = {  }
-    render() { 
-        return ( 
-            <header>
-                <link rel="stylesheet" href="https://unpkg.com/tachyons@4.10.0/css/tachyons.min.css"/>
-                {/* <link href="https://fonts.googleapis.com/css?family=Rubik:300,500,700,900" rel="stylesheet"></link> */}
-                <link href="https://fonts.googleapis.com/css?family=Poppins:300,700" rel="stylesheet"></link>
-                <style jsx global>
-                    {`
-                        body *{
-                            // font-family: 'Rubik', sans-serif;
-                            font-family: 'Poppins', sans-serif;
-                            font-weight: 100;
-                        }
-                    `}
-                </style>
-            </header>
-         );
-    }
-}
- 
 export default Header;
