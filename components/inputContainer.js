be8e4142ee68403a65b532fdaf3aeaa648935fce
Lucas Neumann <lucas.neumann@nubank.com.br>
2018-09-14 21:49:05 -0300

    works!

--- components/inputContainer.js
+++ components/inputContainer.js
@@ -6,51 +6,50 @@ import QuestionChunk from './QuestionChunk';
 class InputContainer extends Component {
   constructor(props) {
     super(props);
-    this.state = {};
+    this.state = { isExpanded: false };
     this.startApp = this.startApp.bind(this);
   }
 
-  startApp() {
-    this.setState({
-    });
-    // document.getElementById('questions').scrollIntoView({ behavior: 'smooth', block: 'start' });
+  startApp = () => {
+    console.log('startapp')
+    this.setState({ isExpanded: true });
   }
 
   render() {
     return (
-          <div className={`w-70 ${this.props.isShowingIntro ? 'h5 debug overflow-hidden' : 'bg-blue'}`}>
-           <div id="questions" className="">
-           <QuestionChunk index="1" title="">
-                <InputField
-                    isCurrency = "false"
-                    value = {this.props.myCurrentAge}
-                    hasSteppers = "true"
-                    stepperIncrement = "1"
-                    min = "1"
-                    max = "100"
-                    id = "myCurrentAge"
-                    label = "Quantos anos você tem?"
-                    handleInput = {this.props.handleInput}
-                    helperText = {this.props.myCurrentAge > 25 ? 'Ainda dá tempo' : 'Começando jovem hein!'}
-                />
-                <InputField
-                    isCurrency = "false"
-                    value = {this.props.myLifeExpectancy}
-                    hasSteppers = "true"
-                    stepperIncrement = "1"
-                    id = "myLifeExpectancy"
-                    label = "Você pretende viver até quantos anos?"
-                    handleInput = {this.props.handleInput}
-                />
-            </QuestionChunk>
-            <QuestionChunk index="2" title="Sobre seu dinheiro">
+      <div id='inputContainer' className={`flex ${ !this.state.isExpanded ? 'h5 overflow-hidden' : 'vh-100'}`}>
+        <div className='flex flex-column'>
+          <QuestionChunk index="1" title="">
+            <InputField
+                isCurrency = "false"
+                value = {this.props.myCurrentAge}
+                hasSteppers = "true"
+                stepperIncrement = "1"
+                min = "1"
+                max = "100"
+                id = "myCurrentAge"
+                label = "Quantos anos você tem?"
+                handleInput = {this.props.handleInput}
+                helperText = {this.props.myCurrentAge > 25 ? 'Ainda dá tempo' : 'Começando jovem hein!'}
+            />
+            <InputField
+                isCurrency = "false"
+                value = {this.props.myLifeExpectancy}
+                hasSteppers = "true"
+                stepperIncrement = "1"
+                id = "myLifeExpectancy"
+                label = "Você pretende viver até quantos anos?"
+                handleInput = {this.props.handleInput}
+            />
+          </QuestionChunk>
+          <QuestionChunk index="2" title="Sobre seu dinheiro">
             <InputField
                 isCurrency = "true"
                 value = {this.props.myCurrentBalance}
                 id = "myCurrentBalance"
                 label = "Quanto você tem hoje para começar a investir?"
                 handleInput = {this.props.handleInput}
-            />
+              />
             <InputField
                 value={this.props.myCurrentMonthlySavings}
                 id = "myCurrentMonthlySavings"
@@ -75,10 +74,20 @@ class InputContainer extends Component {
               handleTableInput = {this.props.handleTableInput}
               handleAddTableRow = {this.props.handleAddTableRow}
               handleRemoveTableRow = {this.props.handleRemoveTableRow}
-              />
-            </QuestionChunk>
-            </div>
-            </div>
+            />
+          </QuestionChunk>
+        </div>
+        <div className="flex flex-column justify-start center">
+          <button className="ph4 pv3 h3 mt5 bg-green white b ttu pointer bn" onClick={this.startApp}>COMEÇAR</button>
+        </div>
+        <style jsx>
+              {`
+              #inputContainer{
+                transition: .2s height
+              }
+              `}
+        </style>
+      </div>
     );
   }
 }
