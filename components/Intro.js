556a068146f382596e695276b2923f225d4353eb
Lucas Neumann <lucas.neumann@nubank.com.br>
2018-09-14 21:06:53 -0300

    one possible path forward

--- components/Intro.js
+++ components/Intro.js
@@ -1,4 +1,5 @@
 import React, { Component } from 'react';
+import InputField from './InputField'
 
 class Intro extends Component {
     state = { }
@@ -10,9 +11,16 @@ class Intro extends Component {
                     <h1 className="b f2 lh-solid">Quero Me  <br />Aposentar</h1>
                     <h2 className="f3 fw1 measure-narrow">Calcule sua independência financeira</h2>
                     <h3 className="f4 measure-narrow gray mb5">Descubra quando você vai poder mandar tudo à merda!</h3>
-                    <button onClick={this.props.handleClick} className="pa4 bg-green white b bn w5 pointer">COMEÇAR</button>
                 </div>
-                <div className="bg-white ph6 pv4 w-100">ahahaha</div>
+                <div className="bg-white ph7 pv4 w-100 flex justify-between items-center">
+                    <InputField
+                        hasSteppers='true'
+                        isCurrency='false'
+                        placeholder='25'
+                        label="Quantos anos você tem?"
+                    />
+                    <button onClick={this.props.handleClick} className="bg-green white b bn pointer ph5 h3">COMEÇAR</button>
+                </div>
             <style jsx>
                 {'.bg-offwhite{background-color: #f8f4ef;}'}
             </style>
