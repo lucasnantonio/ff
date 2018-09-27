1a7794bf555f8aa950ae5c1cccc154c3de3ccb23
Lucas Neumann <lucas.neumann@nubank.com.br>
2018-09-26 21:11:21 -0300

    add initial scale redering rule

--- components/Header.js
+++ components/Header.js
@@ -4,6 +4,7 @@ import Head from 'next/head';
 const Header = () => (
         <Head>
             <title>FF</title>
+            <meta name="viewport" content="width=device-width, initial-scale=1.0" />
             <link href="https://fonts.googleapis.com/css?family=Poppins:200,300,700,900" rel="stylesheet"></link>
             <link rel="stylesheet" href="https://unpkg.com/tachyons@4.10.0/css/tachyons.min.css"/>
 
