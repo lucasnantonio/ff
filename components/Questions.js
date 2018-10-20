b1926e21bd35afd874f86ba8e1a774b884c29f02
Lucas Neumann <lucas.neumann@nubank.com.br>
2018-10-20 13:45:22 -0300

    add button to show chart

--- components/Questions.js
+++ components/Questions.js
@@ -73,6 +73,7 @@ class InputContainer extends Component {
               handleClick={this.props.handleInvestmentSelector}
               hiddenBorder={true}
             />
+            <button onClick={this.props.handleShowCalculation}> Calcular </button>
           </div>
         )}
         {this.state.tabs[1].isSelected && (
