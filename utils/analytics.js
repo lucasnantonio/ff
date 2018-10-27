9f00c4038f384341255675ac2e002580cd952fa4
StollToaldo <guilherme.toaldo@hotmail.com>
2018-10-27 18:03:21 -0300

    remove logs

--- utils/analytics.js
+++ utils/analytics.js
@@ -1,18 +1,19 @@
 import ReactGA from 'react-ga';
 
 export const initGA = () => {
-  console.log('GA init');
+  // console.log('GA init');
   ReactGA.initialize('UA-128217401-1');
 };
 
 export const logPageView = () => {
-  console.log(`Logging pageview for ${window.location.pathname}`);
+  // console.log(`Logging pageview for ${window.location.pathname}`);
   ReactGA.set({ page: window.location.pathname });
   ReactGA.pageview(window.location.pathname);
 };
 
 export const logEvent = (category = '', action = '', label, value) => {
   if (category && action) {
+    // console.log(`category: ${category}, action: ${action}, label: ${label}, value: ${value}`);
     ReactGA.event({
       category, action, label, value,
     });
