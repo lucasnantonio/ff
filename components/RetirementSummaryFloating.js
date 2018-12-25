9317b813c1a517c711de6b6e7ea8c898779754f1
Lucas Neumann <lucas.neumann@nubank.com.br>
2018-12-25 13:00:04 -0200

    floating answer component

---
+++ components/RetirementSummaryFloating.js
@@ -0,0 +1,28 @@
+import React, { Component } from 'react';
+import { formatAge } from '../utils/math';
+
+class RetirementSummaryFloating extends Component {
+  state = {};
+
+  getSelectedInvestmentIndex = (array) => {
+    const selectedItem = array.filter(item => item.isSelected);
+    return array.indexOf(selectedItem[0]);
+  };
+
+  render() {
+    const { age, balance } = this.props.retirementResults[
+      this.getSelectedInvestmentIndex(this.props.myInvestments)
+    ][1].retirement;
+    const [y, m] = formatAge(age);
+
+    return (
+      <div className="fixed w-100 pa4 bg-blue white z-max">
+        <div className="mw7 center">
+          <span className="normal "> Você vai se aposentar aos {y} anos. </span>
+        </div>
+      </div>
+    );
+  }
+}
+
+export default RetirementSummaryFloating;
