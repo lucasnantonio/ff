812fd151755bea365b71f714f441d648b60638fb
Lucas Neumann <lucas.neumann@nubank.com.br>
2018-12-26 00:50:22 -0200

    internalise intl currency

---
+++ utils/formatcurrency.js
@@ -0,0 +1,8 @@
+import Big from 'big.js';
+
+export default function formatCurrency(value, localeConfig, currencyName) {
+  const numberConfig = localeConfig.formats.number[currencyName];
+  const formatter = new global.Intl.NumberFormat(localeConfig.locale, numberConfig);
+
+  return formatter.format(Big(value));
+}
