10a98a1a93957d67bec6fb0ff7fd303bc9d82223
Lucas Neumann <lucas.neumann@nubank.com.br>
2018-12-26 14:30:00 -0200

    create colors file

---
+++ components/Colors.js
@@ -0,0 +1,12 @@
+const colors = {
+  lightPink: '#FFEFE5',
+  mediumPink: '#FFC1A7',
+  redPink: '#F18181',
+  lightBrown: '#cdb7af',
+  darkBrown: '#453e28',
+  darkGreen: '#1f9465',
+  lightYellow: '#f9db79',
+  darkYellow: '#f0c646',
+};
+
+export default colors;
