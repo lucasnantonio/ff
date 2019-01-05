a78a0edc4f632b2cf4c19f8c08babd0b0f9c44eb
Lucas Neumann <lucas.neumann@nubank.com.br>
2019-01-05 15:56:25 -0200

    new color scheme

--- components/Colors.js
+++ components/Colors.js
@@ -1,15 +1,15 @@
 const colors = {
   white: '#fff',
-  mediumPink: '#FFC1A7',
-  redPink: '#F18181',
-  lightBrown: '#586E6E',
-  darkBrown: '#453e28',
-  darkGreen: '#92f8a7',
-  lightGreen: '#0CBA7B',
-  lightYellow: '#f9db79',
-  darkYellow: '#f0c646',
-  gray: '#586E6E',
-  black: '#0C292E',
+  mediumPink: '#fed0d3',
+  redPink: '#fc579a',
+  darkGreen: '#1ED090',
+  darkGreenHover: '#47e0a7',
+  darkGreenActive: '#26af7a',
+  lightGreen: '#f0fbf7',
+  lightGray: '#f6f9fa',
+  lightGray2: '#E5EFF2',
+  mediumGray: '#586E6E',
+  black: '#0d292d',
 };
 
 export default colors;
