4a5bb0568e92f49b537c1adaed204d0df0b0106b
StollToaldo <guilherme.toaldo@hotmail.com>
2019-01-16 20:21:28 -0200

    config env

--- next.config.js
+++ next.config.js
@@ -1,16 +1,32 @@
+const webpack = require('webpack');
+require('dotenv').config();
+
+// module.exports = {
+//   webpack(cfg) {
+//     const originalEntry = cfg.entry;
+//     cfg.entry = async () => {
+//       const entries = await originalEntry();
+
+//       if (entries['main.js'] && !entries['main.js'].includes('./client/polyfills.js')) {
+//         entries['main.js'].unshift('./client/polyfills.js');
+//       }
+
+//       return entries;
+//     };
+
+//     return cfg;
+//   },
+// };
+
 module.exports = {
-  webpack(cfg) {
-    const originalEntry = cfg.entry;
-    cfg.entry = async () => {
-      const entries = await originalEntry();
+  webpack: (config) => {
+    const env = Object.keys(process.env).reduce((acc, curr) => {
+      acc[`process.env.${curr}`] = JSON.stringify(process.env[curr]);
+      return acc;
+    }, {});
 
-      if (entries['main.js'] && !entries['main.js'].includes('./client/polyfills.js')) {
-        entries['main.js'].unshift('./client/polyfills.js');
-      }
+    config.plugins.push(new webpack.DefinePlugin(env));
 
-      return entries;
-    };
-
-    return cfg;
+    return config;
   },
 };
