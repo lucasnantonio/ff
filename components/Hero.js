f4dfd6964c446397a455a478f6e86d0c6068b780
lucasnantonio <lucasneumann.fau@gmail.com>
2020-03-23 16:12:15 -0400

    styles

--- components/Hero.js
+++ components/Hero.js
@@ -13,17 +13,14 @@ const Hero = props => (
         exit={{ opacity: 0, y: -500 }}
         id="hero"
         style={{ backgroundColor: "#CCEEFD" }}
-        className="h-100 ph3"
+        className="h-100 ph3-ns"
       >
-        <div className="mw7-ns center flex flex-row-ns flex-column items-center justify-center h-100 pb0-ns pv5-ns pv4">
+        <div className="mw7-ns center flex flex-row-ns flex-column h-100 pb0-ns pv5-ns pv4">
           <div className="w-60-ns w-100 tl-ns tc mb5-ns">
-            <h1 className="black-80 f2-ns f3 lh-solid normal mt0 mb0 mb3-ns b">
+            <h1 className="black-80 f2-ns f3 lh-title normal mt0 mb0 mb3-ns b mh0-ns mh3">
               Descubra quando você será livre financeiramente.
             </h1>
-            <p
-              className="measure lh-copy f5 dib-ns dn"
-              style={{ color: "rgba(0,0,0,.8)" }}
-            >
+            <p className="measure lh-copy f5 dib-ns dn black-50">
               Começar hoje, mesmo com pouco, é o melhor jeito de garantir seu
               sossego no futuro.
             </p>
@@ -31,19 +28,19 @@ const Hero = props => (
             {/* {!props.isShowingQuestions && ( */}
             <button
               style={{
-                backgroundColor: "#F7CBDC",
-                color: "black",
+                backgroundColor: "#41b9ff",
+                color: "white",
                 fontFamily: "Chocolate Bold, system-ui, sans-serif"
               }}
               onClick={props.startApp}
-              className="fw6 ba0 mt4 ph4 pv3-ns pv4 br-pill-l pointer f5 relative-ns fixed bottom-0 r0 l0 w-auto-ns w-100 b"
+              className="z-max fw6 ba0 mt4 ph4 pv3-ns pv4 br-pill-l pointer f5 relative-ns fixed bottom-0 r0 l0 w-auto-ns w-100 b"
             >
               Começar
             </button>
           </div>
-          <div className="w-60-ns w-100 pl4-l">
-            {/* <img className="w-100" src="../static/3dpig.png"></img> */}
+          <div className="relative h-100 w-100 pl4-l overflow-visible">
             <iframe
+              className="absolute w-100"
               // style={{ marginTop: "-400px" }}
               id="d0e763cb-faed-49a2-9e5e-8ed9524593ce"
               src="https://www.vectary.com/viewer/v1/?model=d0e763cb-faed-49a2-9e5e-8ed9524593ce&env=theskyisonfire&turntable=0.3&showInteractionPrompt=0"
